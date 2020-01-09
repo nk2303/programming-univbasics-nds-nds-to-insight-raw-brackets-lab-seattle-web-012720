67980fcc7ab454fb6eabfc80a9e33a640a535ed7
Ngan Kim Khong <ngankhong03@gmail.com>
2020-01-09 07:28:53 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,8 +4,8 @@ require 'directors_database'
 def directors_totals(nds)
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   #hash[:key] = "value"
-    hash = {}
-    return hash
+  hash = {}
+  return hash
 end
 
 
