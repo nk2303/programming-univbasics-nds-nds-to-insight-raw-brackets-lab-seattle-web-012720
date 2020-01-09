5d2397a784aa1ab5ee5ca745e8b3dbb9c3329b66
Ngan Kim Khong <ngankhong03@gmail.com>
2020-01-09 07:35:55 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
+#[{:name, :movie=>[{:worldwide_gross},{:worldwide_gross}]}]
 
 def directors_totals(nds)
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
