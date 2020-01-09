1789415e47a22af4dc4a4d54380bfbe1c4014052
Ngan Kim Khong <ngankhong03@gmail.com>
2020-01-09 08:07:42 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -6,10 +6,11 @@ def directors_totals(nds)
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   #hash[:key] = "value"
   hash = {}
-  nds.length.times do |namehash|
-    key = nds[namehash][:name]
+  nds.length.times do |nameHash|
+    key = nds[nameHash][:name]
     total = 0
-    nds[namehash][:movies]
+    nds[namehash][:movies] do |totalHash|
+      total = total +=
     
     hash[key] = total
   end
