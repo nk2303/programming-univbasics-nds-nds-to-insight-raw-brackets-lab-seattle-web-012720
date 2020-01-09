2fc19860969c3fc937266b18aadc5ba564cd39c8
Ngan Kim Khong <ngankhong03@gmail.com>
2020-01-09 07:35:29 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,7 +5,8 @@ def directors_totals(nds)
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   #hash[:key] = "value"
   hash = {}
-  
+  nds.length.times do |namehash|
+    
   return hash
 end
 
